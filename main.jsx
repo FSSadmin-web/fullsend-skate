f0abf81717796faa0e82e2904f993e9e9c04d220
FSSadmin-web <hoffmanhammer@gmail.com>
2025-07-15 10:07:08 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import FullSendSkateApp from './FullSendSkateApp.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FullSendSkateApp />
+  </React.StrictMode>
+);
